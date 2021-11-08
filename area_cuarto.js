4fa111416d5e98ce09e093a67550f1587bdedcc6
Daniela-ber <91279841+daniela-ber@users.noreply.github.com>
2021-11-08 23:06:42 +0000

    .

---
+++ area_cuarto.js
@@ -0,0 +1,13 @@
+function calcularArea() 
+
+var nombreDeVariable; 
+var largo; 
+var ancho; 
+var altura; 
+largo = parseFloat(document.getElementById("largo").value;) 
+ancho = parseFloat(document.getElementById("ancho").value;) 
+ alert("El area es=" + area ); 
+
+
+
+
--- promedio.js
+++ promedio.js
@@ -1,25 +1,26 @@
 //Este script realiza el calculo de un promedio de 5 calificaciones
+// 1.Inicio
 //Autor: Daniela Bernardino
 //Fecha: 11 de octubre de 2021
-function calcularPromedio () {
-// 1.Inicio
-// 2.Definir c1, c2 ,c3, c4, c5 promedio
+function calcularPromedio () //Calula promedio
+// 2.Definir c1, c2 ,c3, c4, c5 promediovar
 var c1;
 var c2;
 var c3;
 var c4;
 var c5;
 var promedio; 
+
 // 3. Hacer c1 = 10, c2 = 7.5, c3 = 9, c4 = 10, c5 = 10
-c1 = parseFloat(document.getElementByeId("calificacion1").value);
-c2 = parseFloat(document.getElementByeId("calificacion2").value);
-c3 = parseFloat(document.getElementByeId("calificacion3").value);
-c4 = parseFloat(document.getElementByeId("calificacion4").value);
-c5 = parseFloat(document.getElementByeId("calificacion5").value);
+c1 = parseFloat(document.getElementById("calificacion1").value);
+c2 = parseFloat(document.getElementById("calificacion2").value);
+c3 = parseFloat(document.getElementById("calificacion3").value);
+c4 = parseFloat(document.getElementById("calificacion4").value);
+c5 = parseFloat(document.getElementById("calificacion5").value);
 // 4. Hacer promedio = c1+c2+c3+c4+c5 / 5 = 46.5 / 5 = 9.3
 promedio = (c1 + c2 + c3 + c4 + c5) / 5;
 // 5. Imprimir promedio 
 alert("Tu promedio es:" + promedio);
-}// 6. Fin 
+// 6. Fin 
 
 //calcularPromedio();
